503a09e95f47a9ca38863db2f2bfec474611056e
Ahmed Esam <ahmedesam415@gmail.com>
2025-12-02 01:11:42 +0200

    edit db

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -2,60 +2,10 @@ import { Box, Paper, Stack } from '@mui/material';
 import './main.scss';
 import { useState } from 'react';
 import { Category } from '@mui/icons-material';
+import { myProjects } from './myProjects';
 
 // eslint-disable-next-line no-unused-vars
 import { AnimatePresence, easeInOut, motion } from 'motion/react';
-
-const myProjects = [
-  {
-    id: 1,
-    title: 'react Project',
-    paragraph:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolore aspernatur',
-    Category: 'react',
-    img: '/public/ahmedEsam.jpg',
-  },
-  {
-    id: 2,
-    title: 'Css Project',
-    paragraph:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolore aspernatur',
-    Category: 'css',
-    img: '/public/ahmedEsam.jpg',
-  },
-  {
-    id: 3,
-    title: 'JavaScript Project',
-    paragraph:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolore aspernatur',
-    Category: 'js',
-    img: '/public/ahmedEsam.jpg',
-  },
-  {
-    id: 4,
-    title: 'Node Project',
-    paragraph:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolore aspernatur',
-    Category: 'node',
-    img: '/public/ahmedEsam.jpg',
-  },
-  {
-    id: 5,
-    title: 'Css Project 2',
-    paragraph:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolore aspernatur',
-    Category: ['css', 'react'],
-    img: '/public/ahmedEsam.jpg',
-  },
-  {
-    id: 6,
-    title: 'Boot Strap Project',
-    paragraph:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit.Dolore aspernatur',
-    Category: ['css', 'bootstrap'],
-    img: '/public/ahmedEsam.jpg',
-  },
-];
 
 const Main = ({ mode }) => {
   //# useState
@@ -131,83 +81,93 @@ const Main = ({ mode }) => {
           {project.map((item, i) => {
             const isNew = i === project.length - 1;
 
-            return (
-              //! Start Filter Transition
-              <MotionPaper
-                layout
-                initial={isNew ? { scale: 0.8, y: 20, opacity: 0 } : false}
-                animate={isNew ? { scale: 1, y: 0, opacity: 1 } : false}
-                transition={
-                  isNew
-                    ? {
-                        type: 'spring',
-                        stiffness: 260,
-                        damping: 20,
-                        mass: 0.8,
-                      }
-                    : false
-                }
-                exit={isNew ? { opacity: 0, y: -50 } : false}
-                key={i}
-                className="Card"
-              >
-                <img
-                  src={item.img}
-                  style={{ width: '100%' }}
-                  width={160}
-                  height={160}
-                  alt=""
-                />
-                <Box sx={{ pl: 1, my: 2 }}>
-                  <h1>{item.title}</h1>
-                  <p
-                    style={{
-                      color:
-                        mode === 'dark'
-                          ? 'var(--supTitleInDark)'
-                          : 'var(--supTitleInLight)',
-                    }}
-                  >
-                    {item.paragraph}
-                  </p>
+            if (item.title != '') {
+              return (
+                //! Start Filter Transition
+                <MotionPaper
+                  layout
+                  initial={isNew ? { scale: 0.8, y: 20, opacity: 0 } : false}
+                  animate={isNew ? { scale: 1, y: 0, opacity: 1 } : false}
+                  transition={
+                    isNew
+                      ? {
+                          type: 'spring',
+                          stiffness: 260,
+                          damping: 20,
+                          mass: 0.8,
+                        }
+                      : false
+                  }
+                  exit={isNew ? { opacity: 0, y: -50 } : false}
+                  key={i}
+                  className="Card"
+                >
+                  <img
+                    src={item.img}
+                    style={{ width: '100%' }}
+                    width={160}
+                    height={160}
+                    alt=""
+                  />
+                  <Box sx={{ pl: 1, my: 2 }}>
+                    <h1>{item.title}</h1>
+                    <p
+                      style={{
+                        color:
+                          mode === 'dark'
+                            ? 'var(--supTitleInDark)'
+                            : 'var(--supTitleInLight)',
+                      }}
+                    >
+                      {item.text}
+                    </p>
 
-                  <Stack
-                    component={'nav'}
-                    direction={'row'}
-                    justifyContent={'space-between'}
-                  >
-                    <Box className="navIcons">
-                      <button
-                        style={{
-                          backgroundColor:
-                            mode == 'light'
-                              ? 'var(--supTitleInDark)'
-                              : 'var(--supTitleInLight)',
-                        }}
-                      >
-                        <a className="icon-link" href=""></a>
-                      </button>
+                    <Stack
+                      component={'nav'}
+                      direction={'row'}
+                      justifyContent={'space-between'}
+                    >
+                      <Box className="navIcons">
+                        <button
+                          style={{
+                            backgroundColor:
+                              mode == 'light'
+                                ? 'var(--supTitleInDark)'
+                                : 'var(--supTitleInLight)',
+                          }}
+                        >
+                          <a
+                            className="icon-link"
+                            target="_blank"
+                            href={item.linkProject}
+                          ></a>
+                        </button>
 
-                      <button
-                        style={{
-                          backgroundColor:
-                            mode == 'light'
-                              ? 'var(--supTitleInDark)'
-                              : 'var(--supTitleInLight)',
-                        }}
-                      >
-                        <a className="icon-github" href=""></a>
-                      </button>
-                    </Box>
+                        <button
+                          style={{
+                            backgroundColor:
+                              mode == 'light'
+                                ? 'var(--supTitleInDark)'
+                                : 'var(--supTitleInLight)',
+                          }}
+                        >
+                          <a
+                            className="icon-github"
+                            target="_blank"
+                            href={item.linkGitHub}
+                          ></a>
+                        </button>
+                      </Box>
 
-                    <Box className="naveBtn">
-                      <button>More</button>
-                      <a href="#" className="icon-arrow-right2" />
-                    </Box>
-                  </Stack>
-                </Box>
-              </MotionPaper>
-            );
+                      <Box className="naveBtn">
+                        <button>More</button>
+                        <a href="#" className="icon-arrow-right2" />
+                      </Box>
+                    </Stack>
+                  </Box>
+                </MotionPaper>
+              );
+            }
           })}
         </AnimatePresence>
       </Stack>
